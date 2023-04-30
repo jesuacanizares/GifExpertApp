ec882f13fa30b597d737eda874c5f11882d9e72c
Jesua Canizares <jesuajoel@outlook.es>
2023-04-30 15:01:54 -0500

    Se reduce el numero de imágenes para verlo mejor en el celular

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-    const limit = 10;
+    const limit = 3;
     const APIKEY = "E6XAW9f7YXJDruLPOqpxQQNcE0W9nNL2";
     const URL = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${category}&limit=${limit}`
     const rs = await fetch(URL)
